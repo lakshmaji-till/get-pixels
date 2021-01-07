e9a5e2a781102f17558e855100e199b4c7d2d304
Lakshmaji <lakshmaji@tillpos.com.au>
2021-01-07 19:27:42 +0530

    remove ndarray-pack

--- node-pixels.js
+++ node-pixels.js
@@ -1,12 +1,8 @@
 'use strict'
 
 var ndarray       = require('ndarray')
-var path          = require('path')
 var PNG           = require('pngjs').PNG
 var jpeg          = require('jpeg-js')
-var pack          = require('ndarray-pack')
-var GifReader     = require('omggif').GifReader
-var Bitmap        = require('node-bitmap')
 var fs            = require('fs')
 var request       = require('request')
 var mime          = require('mime-types')
@@ -44,65 +40,6 @@ function handleJPEG(data, cb) {
   cb(null, result.transpose(1,0))
 }
 
-function handleGIF(data, cb) {
-  var reader
-  try {
-    reader = new GifReader(data)
-  } catch(err) {
-    cb(err)
-    return
-  }
-  if(reader.numFrames() > 0) {
-    var nshape = [reader.numFrames(), reader.height, reader.width, 4]
-    try  {
-      var ndata = new Uint8Array(nshape[0] * nshape[1] * nshape[2] * nshape[3])
-    } catch(err) {
-      cb(err)
-      return
-    }
-    var result = ndarray(ndata, nshape)
-    try {
-      for(var i=0; i<reader.numFrames(); ++i) {
-        reader.decodeAndBlitFrameRGBA(i, ndata.subarray(
-          result.index(i, 0, 0, 0),
-          result.index(i+1, 0, 0, 0)))
-      }
-    } catch(err) {
-      cb(err)
-      return
-    }
-    cb(null, result.transpose(0,2,1))
-  } else {
-    var nshape = [reader.height, reader.width, 4]
-    var ndata = new Uint8Array(nshape[0] * nshape[1] * nshape[2])
-    var result = ndarray(ndata, nshape)
-    try {
-      reader.decodeAndBlitFrameRGBA(0, ndata)
-    } catch(err) {
-      cb(err)
-      return
-    }
-    cb(null, result.transpose(1,0))
-  }
-}
-
-function handleBMP(data, cb) {
-  var bmp = new Bitmap(data)
-  try {
-    bmp.init()
-  } catch(e) {
-    cb(e)
-    return
-  }
-  var bmpData = bmp.getData()
-  var nshape = [ bmpData.getHeight(), bmpData.getWidth(), 4 ]
-  var ndata = new Uint8Array(nshape[0] * nshape[1] * nshape[2])
-  var result = ndarray(ndata, nshape)
-  pack(bmpData, result)
-  cb(null, result.transpose(1,0))
-}
-
-
 function doParse(mimeType, data, cb) {
   switch(mimeType) {
     case 'image/png':
@@ -112,14 +49,6 @@ function doParse(mimeType, data, cb) {
     case 'image/jpg':
     case 'image/jpeg':
       handleJPEG(data, cb)
-    break
-
-    case 'image/gif':
-      handleGIF(data, cb)
-    break
-
-    case 'image/bmp':
-      handleBMP(data, cb)
     break
 
     default:
